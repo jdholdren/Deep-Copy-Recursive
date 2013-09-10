67593bed2ea48bc94cab4ca3339187377b7aa016
James Holdren <jamesdholdren@gmail.com>
2013-09-10 09:35:39 -0400

    Initial

---
+++ function.js
@@ -0,0 +1,23 @@
+/**
+* Creates a copy of an object
+* @param the obj to copy
+* @return the copy of the object
+**/
+function copy(obj)
+{
+	var copyObj = {};
+
+	for (var i in obj)
+	{
+		if (typeof(obj[i]) !== 'object')
+		{
+			copyObj[i] = obj[i];
+		}
+		else
+		{
+			copyObj[i] = this.copy(obj[i]);
+		}
+	}
+
+	return copyObj;
+}
